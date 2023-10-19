14f79e22c2fc1b0afffd59d4fc2ed7bbd777b43e
Cody Reynolds <codyrey@buffalo.edu>
2023-10-19 03:31:17 -0400

    Added two interactions to the enviornment in the kitchen and navigation to different rooms

--- Cat1.gd
+++ Cat1.gd
@@ -6,8 +6,10 @@ extends Sprite2D
 
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+	if event is InputEventScreenTouch:
+		if  event.pressed == true:
 			$cat_ani.play()
 			$Bubble.hide()
+			await get_tree().create_timer(2.0).timeout
+			$cat_ani.stop()
 			
