211e955cade00c2165aaa986248f762f8bb3686c
jkohny10 <jkohny@outlook.com>
2023-10-20 21:04:18 -0400

    once again redoing my stuff cuz Godot and Github don't like working together

--- Navigation.gd
+++ Navigation.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 
-func _on_navigate_to_cafe_pressed():
+func _on_nav_to_cafe_pressed():
 	get_tree().change_scene_to_file("res://mainCafe/cafeScreen.tscn")
 
 
-func _on_navigate_to_shop_pressed():
+func _on_nav_to_shop_pressed():
 	get_tree().change_scene_to_file("res://Cat Shop/Cat Shop.tscn")
--- ShopButton.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-
-func _on_shop_button_pressed():
-	get_tree().change_scene_to_file("res://Cat Shop/Cat Shop.tscn")
