a72c1c475df659301ea1d80b56d40805e605d39c
Cody Reynolds <codyrey@buffalo.edu>
2023-11-03 19:39:02 -0400

    fixed merge conflicts in Cat Shop folder

--- Cat Shop/Burmese Cat.gd
+++ Cat Shop/Burmese Cat.gd
@@ -1,4 +1,12 @@
 extends Button
 
+func _ready():
+	$".".disabled = Global.shop_items["Burmese"]
+
 func _on_pressed():
-	$".".disabled = true
+	if Global.cat_coins >= 1750:
+		Global.cat_coins = Global.cat_coins - 1750
+		Global.shop_items["Burmese"] = true
+		$".".disabled = Global.shop_items["Burmese"]
+	else:
+		print(Global.cat_coins)
--- Cat Shop/Himalayan Cat.gd
+++ Cat Shop/Himalayan Cat.gd
@@ -1,4 +1,12 @@
 extends Button
 
+func _ready():
+	$".".disabled = Global.shop_items["Himalayan"]
+
 func _on_pressed():
-	$".".disabled = true
+	if Global.cat_coins >= 1500:
+		Global.cat_coins = Global.cat_coins - 1500
+		Global.shop_items["Himalayan"] = true
+		$".".disabled = Global.shop_items["Himalayan"]
+	else:
+		print(Global.cat_coins)
