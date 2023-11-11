cd76f6fdb5f8fecc5718c36f112f371fbca25377
Cody Reynolds <codyrey@buffalo.edu>
2023-11-11 13:00:23 -0500

    pushed garden mini-game to dev -Amit

---
+++ Garden Screen/TouchScreenButton3.gd
@@ -0,0 +1,6 @@
+extends TouchScreenButton
+
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Garden Screen/plant_shop.tscn")
---
+++ plant_game.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+var tomato = false
+var carrot = false
+var onion = false
+var potato = false
+var cucumber = false
+var corn = false
+
+
+
+
+
