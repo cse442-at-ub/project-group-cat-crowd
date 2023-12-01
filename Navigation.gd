739c44c42d8401846cdd08eb1543b3fce1e861ba
Nucherino <pennacchiodominic@gmail.com>
2023-12-01 14:26:28 -0500

    Added identifiers for the navigation options, so that the user can better tell how to chnage pages

--- Navigation.gd
+++ Navigation.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
-
 func _on_nav_to_cafe_pressed():
 	get_tree().change_scene_to_file("res://mainCafe/cafeScreen.tscn")
-
 
 func _on_nav_to_shop_pressed():
 	get_tree().change_scene_to_file("res://Cat Shop/Cat Shop.tscn")
