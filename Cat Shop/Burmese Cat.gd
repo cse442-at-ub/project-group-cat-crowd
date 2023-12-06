3b77af546045ec22f61cbacc0ad1729cbfd0ea53
gigiokt <gokaitey@buffalo.edu>
2023-12-06 12:28:57 -0500

    Revert "added missing elements"
    This reverts commit 5deb7b79ba346e474edc1793df3452ca72ce8735.

--- Cat Shop/Burmese Cat.gd
+++ Cat Shop/Burmese Cat.gd
@@ -7,7 +7,6 @@ func _on_pressed():
 	if Global.cat_coins >= 1750:
 		Global.cat_coins = Global.cat_coins - 1750
 		Global.cat_purchased["Burmese"] = true
-		Global.catsAvailable[0] = true
 		$".".disabled = Global.cat_purchased["Burmese"]
 	else:
 		$"../../CurrencyPopup".visible = true
