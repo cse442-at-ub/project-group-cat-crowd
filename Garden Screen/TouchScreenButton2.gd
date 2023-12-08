46c4f8409b6b8fc5b11808fd8052a023913bf576
Cody Reynolds <codyrey@buffalo.edu>
2023-12-08 16:59:18 -0500

    added the final phase for the rest of the vegetables, and added price tag to plant shop. -Amit

--- Garden Screen/TouchScreenButton2.gd
+++ Garden Screen/TouchScreenButton2.gd
@@ -2,10 +2,10 @@ extends TouchScreenButton
 
 
 func _on_pressed():
-	if not $onion_plant.visible and not $onion_plant2.visible and not $onion_plant3.visible:
+	if not $onion_plant.visible and not $onion_plant2.visible and not $onion_plant3.visible and not $onion_plant4.visible:
 		get_tree().change_scene_to_file("res://Garden Screen/plant_shop.tscn")
-	elif $onion_plant3.visible:
+	elif $onion_plant4.visible:
 		GlobalVeggies.onion += 1
 		PlantGame.onion = false
 		GardenTimer.onionTimer = 0
-		$onion_plant3.hide()
+		$onion_plant4.hide()
--- Garden Screen/carrot.gd
+++ Garden Screen/carrot.gd
@@ -2,10 +2,10 @@ extends TouchScreenButton
 
 
 func _on_pressed():
-	if not $carrot_plant.visible and not $carrot_plant2.visible and not $carrot_plant3.visible:
+	if not $carrot_plant.visible and not $carrot_plant2.visible and not $carrot_plant3.visible and not $carrot_plant4.visible:
 		get_tree().change_scene_to_file("res://Garden Screen/plant_shop.tscn")
-	elif $carrot_plant3.visible:
+	elif $carrot_plant4.visible:
 		PlantGame.carrot = false
 		GlobalVeggies.carrot += 1
 		GardenTimer.carrTimer = 0
-		$carrot_plant3.hide()
+		$carrot_plant4.hide()
