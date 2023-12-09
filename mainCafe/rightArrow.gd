77d830cdb3b9c2acaa86a3f0dec01a8701dbe928
Nucherino <pennacchiodominic@gmail.com>
2023-12-09 14:32:28 -0500

    Fixed bug where last 2 cats weren't shown on cat equiping system

--- mainCafe/rightArrow.gd
+++ mainCafe/rightArrow.gd
@@ -9,7 +9,7 @@ func _on_pressed():
 	var animated_sprite = $catOptions
 
 	# Increment the frame and wrap it to the range [0, 5]
-	var next = (animated_sprite.frame + 1) % 6
+	var next = (animated_sprite.frame + 1) % 8
 	print(Global.catsAvailable[next])
 	if Global.catsAvailable[next]:
 		$catOptions.visible  = true
